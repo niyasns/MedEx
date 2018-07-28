b06aab4c370c6770137650910ca028b9e13e125a
niyasns <niyasns007@outlook.com>
2018-07-28 23:24:04 +0530

    framework completed

--- app/src/main/java/com/example/android/medex/SignupActivity.java
+++ app/src/main/java/com/example/android/medex/SignupActivity.java
@@ -1,13 +1,105 @@
 package com.example.android.medex;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class SignupActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int RC_SIGN_IN = 0;
+    private static final String TAG = "SignUpActivity";
+    GoogleSignInClient mGoogleSignInClient;
+    Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_activity);
+
+        mSignUpButton = findViewById(R.id.g_signup);
+
+        mSignUpButton.setOnClickListener(this);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestProfile()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        alertIfSignedUpAlready(account);
+    }
+
+    private void alertIfSignedUpAlready(GoogleSignInAccount account) {
+
+        if(account != null)
+        {
+            Toast.makeText(this, "Already Signed up, Please login", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.g_signup:
+                gSignUp();
+                break;
+                
+            case R.id.f_signup:
+                fSignUp();
+                break;
+        }
+    }
+
+    private void fSignUp() {
+    }
+
+    private void gSignUp() {
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN) {
+
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+
+        try{
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            newUserLoggedIn(account);
+
+        }catch (ApiException e) {
+            Log.w(TAG, "signInResult:failed code = " + e.getStatusCode());
+            newUserLoggedIn(null);
+        }
+    }
+
+    private void newUserLoggedIn(GoogleSignInAccount account) {
     }
 }
