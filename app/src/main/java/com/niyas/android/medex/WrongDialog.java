75e21a66648e7bd357c43432c4f111fa12cc9bb6
niyasns <niyasns007@outlook.com>
2018-12-27 10:20:14 +0530

    google services updated

--- app/src/main/java/com/niyas/android/medex/WrongDialog.java
+++ app/src/main/java/com/niyas/android/medex/WrongDialog.java
@@ -2,19 +2,19 @@ package com.niyas.android.medex;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 
 /* Dialog for wrong answer */
-public class WrongDialog extends Dialog implements View.OnClickListener {
+public class WrongDialog extends Dialog {
 
     Activity mActivity;
-    Button close;
 
     public WrongDialog(@NonNull Activity activity) {
         super(activity);
@@ -25,19 +25,27 @@ public class WrongDialog extends Dialog implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        mActivity.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
         setContentView(R.layout.failed_dialog);
-        close = findViewById(R.id.btn_close);
-        close.setOnClickListener(this);
+        Typeface raleway_bold = Typeface.createFromAsset(mActivity.getAssets(),"fonts/Raleway-Bold.ttf" );
+        Typeface raleway_regular = Typeface.createFromAsset(mActivity.getAssets(),"fonts/Raleway-Regular.ttf" );
+        TextView dialogheading, dialogSubheading;
+        dialogheading = (TextView) findViewById(R.id.dailog_heading);
+        dialogSubheading = (TextView) findViewById(R.id.dailog_subheading);
+
+        dialogheading.setTypeface(raleway_bold);
+        dialogSubheading.setTypeface(raleway_regular);
     }
 
-    @Override
+/*    @Override
     public void onClick(View v) {
         dismiss();
-        mActivity.stopService(new Intent(mActivity, BackgroundSoundService.class));
+        //mActivity.stopService(new Intent(mActivity, BackgroundSoundService.class));
         ((AppCompatActivity)mActivity).getSupportFragmentManager().popBackStackImmediate();
         android.support.v4.app.FragmentManager fragmentManager = ((AppCompatActivity)mActivity).getSupportFragmentManager();
         android.support.v4.app.FragmentTransaction  fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_window, new HomeFragment());
         fragmentTransaction.setTransitionStyle(android.support.v4.app.FragmentTransaction.TRANSIT_FRAGMENT_FADE);
         fragmentTransaction.commit();
-    }
+    }*/
 }
