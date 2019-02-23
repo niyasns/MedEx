4bf8a97553c1b065c3dc4098c57aca62d9cc886a
niyasns <niyasns007@outlook.com>
2019-02-23 20:18:07 +0530

    Google server id refactored

--- app/src/main/java/com/niyas/android/medex/SignupActivity.java
+++ app/src/main/java/com/niyas/android/medex/SignupActivity.java
@@ -104,7 +104,7 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         mAuth = FirebaseAuth.getInstance();
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken(getString(R.string.server_client_id))
                 .requestEmail()
                 .build();
 
